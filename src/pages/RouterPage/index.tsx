b2637375eaff4af6b53546ecd16c169e5e85e236
BreathlessWay <731005087@qq.com>
2019-11-21 18:48:20 +0800

    commit-lint待添加

--- src/pages/RouterPage/index.tsx
+++ src/pages/RouterPage/index.tsx
@@ -1,80 +1,80 @@
-import React, { lazy } from "react";
-import { Route, RouteComponentProps, Switch, Redirect } from "react-router-dom";
-import { inject, observer } from "mobx-react";
+import React, { lazy } from 'react';
+import { Route, RouteComponentProps, Switch, Redirect } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
 
-import { Layout } from "antd";
+import { Layout } from 'antd';
 
-import { StoreType } from "@/store/store";
+import { StoreType } from '@/store/store';
 
-import { login } from "@/service/login";
+import { login } from '@/service/login';
 
-import "./style.scss";
+import './style.scss';
 
 const { Header, Content, Footer, Sider } = Layout;
 
 const MenuComponent = lazy(() =>
-  import(
-    /* webpackChunkName: "MenuComponent" */ "../../components/business/MenuComponent"
-  )
+	import(
+		/* webpackChunkName: "MenuComponent" */ '../../components/business/MenuComponent'
+	),
 );
 const BreadcrumbComponent = lazy(() =>
-  import(
-    /* webpackChunkName: "BreadcrumbComponent" */ "../../components/business/BreadcrumbComponent"
-  )
+	import(
+		/* webpackChunkName: "BreadcrumbComponent" */ '../../components/business/BreadcrumbComponent'
+	),
 );
 
 const HomePage = lazy(() =>
-  import(/* webpackChunkName: "HomePage" */ "../HomePage")
+	import(/* webpackChunkName: "HomePage" */ '../HomePage'),
 );
 
 export type IRouterPagePropType = StoreType & RouteComponentProps;
 
 @inject((allStore: StoreType) => ({
-  userStore: allStore.userStore,
-  homepageStore: allStore.homepageStore
+	userStore: allStore.userStore,
+	homepageStore: allStore.homepageStore,
 }))
 @observer
 class RouterPage extends React.Component<IRouterPagePropType> {
-  componentDidMount(): void {
-    const {
-      userStore,
-      history,
-      homepageStore: { firstMenu }
-    } = this.props;
-    login({ userStore, history, firstMenu });
-  }
+	componentDidMount(): void {
+		const {
+			userStore,
+			history,
+			homepageStore: { firstMenu },
+		} = this.props;
+		login({ userStore, history, firstMenu });
+	}
 
-  render() {
-    const {
-      homepageStore: { firstMenu }
-    } = this.props;
-    return (
-      <Layout>
-        <Sider className="home-page_sider">
-          <aside className="home-page_logo">博客管理后台</aside>
-          <MenuComponent />
-        </Sider>
-        <Layout className="home-page_layout">
-          <Header className="home-page_header">
-            <BreadcrumbComponent />
-          </Header>
-          <Content className="home-page_content">
-            <Switch>
-              <Route path="/home" component={HomePage} />
-              <Route path="/me" children={<div>me</div>} />
-              <Route path="/article" children={<div>article</div>} />
-              <Route path="/cat" children={<div>cat</div>} />
-              <Route path="/photography" children={<div>photography</div>} />
-              <Redirect from="/" to={firstMenu.path} />
-            </Switch>
-          </Content>
-          <Footer className="home-page_footer">
-            BreathlessWay 博客管理后台系统
-          </Footer>
-        </Layout>
-      </Layout>
-    );
-  }
+	render() {
+		const {
+			homepageStore: { firstMenu },
+		} = this.props;
+		return (
+			<Layout>
+				<Sider className="home-page_sider">
+					<aside className="home-page_logo">博客管理后台</aside>
+					<MenuComponent />
+				</Sider>
+				<Layout className="home-page_layout">
+					<Header className="home-page_header">
+						<BreadcrumbComponent />
+					</Header>
+					<Content className="home-page_content">
+						<Switch>
+							<Route path="/home" component={HomePage} />
+							<Route path="/me" children={<div>me</div>} />
+							<Route path="/article" children={<div>article</div>} />
+							<Route path="/cat" children={<div>cat</div>} />
+							<Route path="/photography" children={<div>photography</div>} />
+							<Redirect from="/" to={firstMenu.path} />
+						</Switch>
+					</Content>
+					<Footer className="home-page_footer">
+						BreathlessWay 博客管理后台系统
+					</Footer>
+				</Layout>
+			</Layout>
+		);
+	}
 }
 
 export default RouterPage as any;
