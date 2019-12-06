f85b2457e74447af97a624eee56b399b40321e3b
BreathlessWay <731005087@qq.com>
2019-12-06 19:31:17 +0800

    feat: 完成图片分页

--- src/components/business/CatPaginationComponent/index.tsx
+++ src/components/business/CatPaginationComponent/index.tsx
@@ -1,9 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, ComponentClass } from 'react';
 
-class CatPaginationComponent extends Component {
+import { inject, observer } from 'mobx-react';
+
+import { Pagination } from 'antd';
+
+import { StoreType } from '@/store/store';
+
+import './style.scss';
+
+export type CatPaginationComponentPropType = Pick<StoreType, 'catStore'>;
+
+@inject('catStore')
+@observer
+class CatPaginationComponent extends Component<CatPaginationComponentPropType> {
+	handlePaginationChange = (page: number) => {
+		this.props.catStore.jumpToPage(page);
+	};
+
+	handleShowSizeChange = (current: number, size: number) => {
+		this.props.catStore.changePageSize(size);
+	};
+
 	render() {
-		return <div>CatPaginationComponent</div>;
+		const { pageSize, pageIndex, count } = this.props.catStore;
+
+		return (
+			<section className="cat-pagination">
+				<Pagination
+					pageSize={pageSize}
+					current={pageIndex}
+					showQuickJumper={true}
+					showTotal={total => <span>共{total}张图片</span>}
+					total={count}
+					showSizeChanger={true}
+					onChange={this.handlePaginationChange}
+					onShowSizeChange={this.handleShowSizeChange}
+				/>
+			</section>
+		);
 	}
 }
 
-export default CatPaginationComponent;
+export default (CatPaginationComponent as unknown) as ComponentClass;
