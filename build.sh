f2c7eb236f5c12bab38477df5e8d8f84c23cf606
BreathlessWay <731005087@qq.com>
2020-01-20 17:32:12 +0800

    ci: docker compose

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-docker build --rm --no-cache=true -t docker_image_blog_admin . \
-&& docker stop docker_container_blog_admin || true \
-&& docker rm docker_container_blog_admin || true \
-&& docker-compose up -d
+docker-compose up -d
