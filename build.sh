a452db1ce552e13e885807ba1acff540a654e486
BreathlessWay <731005087@qq.com>
2020-01-22 09:16:22 +0800

    fix: 拼写错误

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker-compose build --no-cache --force-rm docker_compose_blog_admim \
-&& docker-compose up -d docker_compose_blog_admim
+docker-compose build --no-cache --force-rm docker_compose_blog_admin \
+&& docker-compose up -d docker_compose_blog_admin
