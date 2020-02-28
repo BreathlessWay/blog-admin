21e4f7360fb25d37b51b94ca9974e658f9307ac7
BreathlessWay <BreathlessWay@users.noreply.github.com>
2020-02-28 06:38:54 +0000

    Automated deployment: Fri Feb 28 06:38:54 UTC 2020 67193cd90338f614f81fde7d9c2aadb0fc3e4fa2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog-admin/precache-manifest.d3cb1fe3f2a44b26ca28efe17b4a4cc1.js"
+  "/precache-manifest.d5092233c3e91c5846675b8025636beb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog-admin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
